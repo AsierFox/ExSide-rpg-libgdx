74f8e878961c7d1c91ac13e72ea1002b806f3498
AsierFox <asigonlo@gmail.com>
2018-11-17 01:33:08 +0100

    Initial code setup

---
+++ desktop/src/com/needsoft/exside/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.needsoft.exside.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.needsoft.exside.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
