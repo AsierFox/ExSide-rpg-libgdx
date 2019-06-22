ac15ef390d679eb25638998e863afcb6a72aa046
AsierFox <asigonlo@gmail.com>
2019-06-22 10:18:02 +0200

    Wrap items to Item class & create HealthPotion

--- core/src/com/needsoft/exside/systems/hud/HUD.java
+++ core/src/com/needsoft/exside/systems/hud/HUD.java
@@ -8,7 +8,6 @@ import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.utils.Queue;
 import com.kotcrab.vis.ui.VisUI;
 import com.kotcrab.vis.ui.widget.VisImage;
 import com.kotcrab.vis.ui.widget.VisProgressBar;
--- desktop/src/com/needsoft/exside/desktop/DesktopLauncher.java
+++ desktop/src/com/needsoft/exside/desktop/DesktopLauncher.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.needsoft.exside.Game;
 
 public class DesktopLauncher {
+
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = Game.WINDOW_WIDTH;
@@ -13,4 +14,5 @@ public class DesktopLauncher {
 		config.title = Game.TITLE;
 		new LwjglApplication(new Game(), config);
 	}
+
 }
